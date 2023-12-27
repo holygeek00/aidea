57760e89f5b6d19047d1301830f0ea29a2ef7056
liuqingfeng <719993988@qq.com>
2023-12-28 01:01:14 +0800

    update api

--- lib/helper/env.dart
+++ lib/helper/env.dart
@@ -1,6 +1,6 @@
 /// 默认 API 服务器地址
 /// 注意：当你使用自己的服务器时，请修改该地址为你自己的服务器地址
-const defaultAPIServerURL = 'http://34.80.104.111';
+const defaultAPIServerURL = 'http://api.yisuanweilai.com';
 
 /// API 服务器地址
 String get apiServerURL {
